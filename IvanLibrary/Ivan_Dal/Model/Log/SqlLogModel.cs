4a64ca5622093366731bd0b6cd98c91c5789e748
harrychen <harrychen1471@gmail.com>
2022-08-26 14:42:41 +0800

    補上SqlLogModel

---
+++ IvanLibrary/Ivan_Dal/Model/Log/SqlLogModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ivan.Models
+{
+    public class SqlLogModel
+    {
+        /// <summary>
+        /// SQL 執行語法
+        /// </summary>
+        public string SQL_TEXT { get; set; }
+
+        /// <summary>
+        /// SQL 變數
+        /// </summary>
+        public string SQL_PARAMETERS { get; set; }
+    }
+}
+
+
